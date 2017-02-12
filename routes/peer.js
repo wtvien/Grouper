66d047b4d5f477fee31a1022a0a7423c24f94a54
Whitney Vien <wtv726@gmail.com>
2017-02-12 15:48:08 -0800

    created popup preview for peer profiles

--- routes/peer.js
+++ routes/peer.js
@@ -1,6 +1,17 @@
 exports.view = function(req, res) {
+  var daysOfWeek = [
+    { day: "Mon", from: "inputMonFrom", to: "inputMonTo" },
+    { day: "Tues", from: "inputTuesFrom", to: "inputTuesTo" },
+    { day: "Wed", from: "inputWedFrom", to: "inputWedTo" },
+    { day: "Thurs", from: "inputThursFrom", to: "inputThursTo" },
+    { day: "Fri", from: "inputFriFrom", to: "inputFriTo" },
+    { day: "Sat", from: "inputSatFrom", to: "inputSatTo" },
+    { day: "Sun", from: "inputSunFrom", to: "inputSunTo" }
+  ];
+
   res.render('peer', {
     title: 'Student ' + req.params.peerId,
-    courseUrl: '/course/' + req.params.courseId
+    courseUrl: '/course/' + req.params.courseId,
+    daysOfWeek: daysOfWeek
   });
 };
