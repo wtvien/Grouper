284bd985501b44cf69d5d290d9d6212b20ca3d8e
David Amadeo <amadeo.k.david@gmail.com>
2017-02-12 21:46:58 -0800

    Added data.json with sample student and course data

--- routes/course.js
+++ routes/course.js
@@ -1,13 +1,13 @@
 exports.view = function(req, res) {
   var courseId = req.params.courseId;
   res.render('course', {
-    title: 'Course ' + courseId,
+    title: courseId,
     peers : [
-      { peerId: 'A', description: 'description A', 
+      { peerId: 'A', description: 'description A',
       		peerUrl: '/course/' + courseId + '/peer/A' },
-      { peerId: 'B', description: 'description B', 
+      { peerId: 'B', description: 'description B',
       		peerUrl: '/course/' + courseId + '/peer/B' },
-      { peerId: 'C', description: 'description C', 
+      { peerId: 'C', description: 'description C',
       		peerUrl: '/course/' + courseId + '/peer/C' }
     ]
    });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,19 @@
+var data = require('../data.json');
+var _ = require('lodash');
+
 /* GET home page. */
 exports.view = function(req, res) {
-  res.render('index', { title: 'Grouper' });
+  // Temporarily using first student as logged in
+  var student = data.students[0];
+  // Load data for each enrolled course
+  var courses = [];
+  for (let courseId of student.enrolled) {
+    let course = _.find(data.courses, {'id': courseId});
+    course.url = encodeURI('/course/' + courseId);
+    courses.push(course);
+  }
+  res.render('index', {
+    title: 'Grouper',
+    courses: courses
+   });
 };
