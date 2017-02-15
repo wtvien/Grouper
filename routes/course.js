deed9b895846acd35ed622d9172f6a106a1b02b6
David Amadeo <amadeo.k.david@gmail.com>
2017-02-14 20:13:21 -0800

    Edited data.json and fixed bugs

--- routes/course.js
+++ routes/course.js
@@ -5,18 +5,19 @@ exports.view = function(req, res) {
   var user = data.students[0];
   // Load data for course
   var courseId = req.params.courseId;
-  var course = data.courses.find(function(c) { return c.id === courseId });
+  var course = data.courses.find(function(c) { return c.id === courseId; });
   // Load data for each student in the course
   var peers = [];
   for (let studentId of course.students) {
     if (studentId === user.id)
       continue;
-    let student = data.students.find(function(s) { return s.id === studentId });
+    let student = data.students.find(function(s) { return s.id === studentId;});
+    let studentCourseData = student.courses.find(function(c) { return c.id === courseId; });
     let peer = {
       name : student.name,
       description : 'description',
       url : encodeURI('/course/' + courseId + '/peer/' + studentId),
-      groupStatus : (student.groups[courseId].length + 1) + '/' + course.groupSize
+      groupStatus : (studentCourseData.group.length + 1) + '/' + course.groupSize
     };
     peers.push(peer);
   }
