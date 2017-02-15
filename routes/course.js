fade6e65b5f5c3ac48a6db8319b8ec8b3a7daa6d
David Amadeo <amadeo.k.david@gmail.com>
2017-02-14 18:39:08 -0800

    Removed lodash

--- routes/course.js
+++ routes/course.js
@@ -1,18 +1,17 @@
 var data = require('../data.json');
-var _ = require('lodash');
 
 exports.view = function(req, res) {
   // Temporarily using first student as logged in user
   var user = data.students[0];
   // Load data for course
   var courseId = req.params.courseId;
-  var course = _.find(data.courses, {'id' : courseId});
+  var course = data.courses.find(function(c) { return c.id === courseId });
   // Load data for each student in the course
   var peers = [];
   for (let studentId of course.students) {
     if (studentId === user.id)
       continue;
-    let student = _.find(data.students, {'id' : studentId});
+    let student = data.students.find(function(s) { return s.id === studentId });
     let peer = {
       name : student.name,
       description : 'description',
