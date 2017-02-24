c82d72188cead7d74907a0555a13fdd26f297fae
David Amadeo <amadeo.k.david@gmail.com>
2017-02-24 11:08:59 -0800

    Disable Join Group button when needed

--- public/js/peer.js
+++ public/js/peer.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+  updateJoinButton();
+  $('#joinGroupBtn').click(updateJoinButton);
+});
+
+function updateJoinButton() {
+  $.get('/data', function(result) {
+    var peer = result.students.find(function(s) { return s.id === peerId; });
+    var course = result.courses.find(function(c) { return c.id === courseId; });
+    if (peer.groups[courseId].length + 1 === course.groupSize)
+      $('#joinGroupBtn').attr('disabled', true);
+  });
+}
+
 function joinGroup() {
   $.post('/course/' + courseId + '/peer/' + peerId);
 }
